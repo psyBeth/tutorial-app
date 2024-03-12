b4425fc858ad4a5dca300dbbc4a136d43027ef00
Betul <betulkoru06@gmail.com>
2024-03-12 21:59:53 +0300

    todo mongoose model backend

--- backend/models/todo.model.js
+++ backend/models/todo.model.js
@@ -1,1 +1,41 @@
 'use strict'
+
+const mongoose = require('mongoose')
+
+const PRIORITIES = {
+    LOW: "low",
+    NORMAL: "normal",
+    HIGH: "high"
+}
+
+const todoSchema = new mongoose.Schema(
+    {
+        title: {
+        type: String,
+        trim: true,
+        required: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        isDone: {
+            type: Boolean,
+            default: false,
+        },
+        priority: {  // low, normal, high
+            type: String,
+            enum: {
+                values: Object.values(PRIORITIES),
+                message: "priorities only can be low, normal or high",
+            },
+        },
+    },{
+    collection:"todo",
+    timestamps:true,
+    },
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports ={Todo}
