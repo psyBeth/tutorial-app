ea0f84c92f9da499978154d89292bcd23ded72e4
Betul <betulkoru06@gmail.com>
2024-03-12 23:06:28 +0300

    todo router backend

--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const express = require("express");
+const { todoController } = require("../controllers/todo.controller");
+const todoRouter = express.Router();
+
+todoRouter.route('/').get(todoController.list).post(todoController.create);
+
+todoRouter.route('/:id')
+    .get(todoController.read)
+    .put(todoController.update)
+    .patch(todoController.update)
+    .delete(todoController.delete)
+
+module.exports = {todoRouter}
