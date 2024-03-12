6cde857246ef93390b2eb9f1be6f557afb02d3ab
Betul <betulkoru06@gmail.com>
2024-03-12 21:42:38 +0300

    errorHandler backend

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const express = require("express");
 const PORT = process.env?.PORT || 8000;
 
 const app = express();
+
+app.use(require('./middlewares/errorHandler'))
+
 mongooseConnection();
 
 app.listen(PORT, () => console.log("Listening http://127.0.0.1:" + PORT));
