99e403b12b3ef137841ec9b4dd707dc6d864ef79
Betul <betulkoru06@gmail.com>
2024-03-12 23:41:49 +0300

    problem on url

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,13 @@ const express = require("express");
 const PORT = process.env?.PORT || 8000;
 
 const app = express();
+
+// app.use({
+//     cors({
+//         origin: "http://localhost:3000",
+//     })
+// })
+
 app.use(express.json())  //parse
 
 app.use(todoRouter);
